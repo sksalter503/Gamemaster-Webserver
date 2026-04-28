b70fdbdc439ee4a82b9d80b5234cd46d45a4f374
sksalter503 <sksalter503@gmail.com>
2026-04-28 10:13:15 -0500

    attempting to fix database

---
+++ src/server/app-data-source.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { InitiativeEntity } from "./entity/initiative.entity";
+import { UserEntity } from "./entity/user.entity";
+
+export const myDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: parseInt(process.env.DB_PORT as string) || 3306,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [InitiativeEntity, UserEntity],
+    synchronize: true,
+});
