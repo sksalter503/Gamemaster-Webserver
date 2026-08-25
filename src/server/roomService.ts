92b66e4de057aedd2f40e283967f2e29bd8701ca
Shane Salter <sksalter503@gmail.com>
2026-08-25 18:05:05 -0500

    fixing the removeInitiativeFromRoom function.

--- src/server/roomService.ts
+++ src/server/roomService.ts
@@ -120,13 +120,13 @@ export function removeInitiativeFromRoom(roomId: string, initiativeId: string): 
         }
 
         //Get the index of the initiative in the room's initiatives array
-        const initiativeIndex = room.initiatives.findIndex(init => init.id === initiativeId);
+        const initiativeIndex = room.initiatives.sort((a, b) => b.initiative - a.initiative).findIndex(init => init.id = initiativeId);
         room.initiatives = room.initiatives.filter(init => init.id !== initiativeId);
 
         if (initiativeIndex < room.turnIndex) {
             room.turnIndex--;
         }
-        if (room.turnIndex >= room.initiatives.length) {
+        if ((room.turnIndex >= room.initiatives.length) || (room.turnIndex < 0)) {
             room.turnIndex = 0; // Reset to 0 if the current turn index exceeds the number of initiatives
         }
         if (room.initiatives.length === 0) {
